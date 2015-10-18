6b6caa18b35024ab5e204ae9deffcba40d207e31
Nick Sanford <nicholascsanford@gmail.com>
2015-10-18 14:54:44 -0400

    Commit 2

--- app.rb
+++ app.rb
@@ -1,6 +1,6 @@
 module App
   def self.call(env)
     puts Rack::Request.new(env).params
-    [200, { 'Content-Type' => 'text/plain' }, StringIO.new(Rack::Request.new(env).params.to_s)] 
+    [200, { 'Content-Type' => 'text/plain' }, StringIO.new(Rack::Request.new(env).params.to_s)]  
   end
 end
