b8f7169b7e5bb5eaf69cf594ff70b745c2bb92a6
Nick Sanford <nicholascsanford@gmail.com>
2015-10-25 14:55:12 -0400

    Re-Organize code and add rspec

--- app.rb
+++ app.rb
@@ -1,67 +1,58 @@
 require 'octokit'
 require 'rdiscount'
-require 'pry'
+require './lib/wrappers/github'
+require './lib/parsers'
+require './lib/constants'
+
 module App
-  CLIENT            = Octokit::Client.new(access_token: ENV['ACCESS_TOKEN']) 
-  PAGES_REPO_NAME   = ENV['PAGES_REPO_NAME']
-  PAGES_BRANCH      = CLIENT.branch(PAGES_REPO_NAME, 'gh-pages')
-  DEPLOY_REPO_NAME  = ENV['DEPLOY_REPO_NAME']
-  MARKDOWN_FILENAME = ENV['MARKDOWN_FILENAME']
-  HTML_FILENAME     = ENV['HTML_FILENAME']
+  include Constants
+  include Parsers
+  include Wrappers::Github
 
   def self.call(env)
     puts Rack::Request.new(env).params
     if deploy_commit_sha = Rack::Request.new(env).params['head_long']
-      puts deploy_commit_sha
-      body = fetch_body_from_commit(deploy_commit_sha)
-      puts body 
-      puts fetch_old_index_markdown
-      index_markdown = add_body(fetch_old_index_markdown, body, current_now_string).join("\n\n")
-      puts index_markdown
-      update_files index_markdown
-      success_response
+      update_log_with_pull_request_message(deploy_commit_sha)
     else
-      error_response
+      [500, { 'Content-Type' => 'text/html' }, ['Error']]
     end
   end
 
+  def self.update_log_with_pull_request_message(deploy_commit_sha)
+    old_index_markdown_hash = contents_hash(PAGES_REPO_NAME, MARKDOWN_FILENAME)
+    index_markdown = insert_body_into_markdown(old_index_markdown_hash[:contents],
+                              fetch_body_array(deploy_commit_sha),
+                              current_now_string)
+    update_markdown index_markdown, old_index_markdown_hash[:sha]
+    update_html index_markdown
+    [200, { 'Content-Type' => 'text/html' }, []]
+  end
+
   private
-  def self.fetch_body_from_commit(commit_sha)
-    commit_message = CLIENT.commit(DEPLOY_REPO_NAME, commit_sha).commit.message
-    CLIENT.pull_request(DEPLOY_REPO_NAME, parse_pull_request_number(commit_message)).body
-  end
-
-  def self.fetch_old_index_markdown
-    Base64.decode64 CLIENT.contents(PAGES_REPO_NAME, path: MARKDOWN_FILENAME).content
-  end
-
-  def self.update_files(index_markdown)
-    File.write(MARKDOWN_FILENAME, index_markdown)
-    File.write(HTML_FILENAME, RDiscount.new(index_markdown).to_html)
-  end
-
-  def self.parse_pull_request_number message_string
-    message_string.split(' ').detect { |word| word.include? '#' }.match(/\d+/).to_s.to_i
-  end
-
   def self.current_now_string
     Time.now.strftime('%m/%d/%Y - %A')
   end
 
-  def self.add_body(lines, body, now_string)
-    body_array = body.split("\n").reject { |word| word == "" }
+  def self.fetch_body_array(commit_sha)
+    pr_number = parse_pull_request_number commit_message(DEPLOY_REPO_NAME, commit_sha)
+    parse_lines pr_body(DEPLOY_REPO_NAME, pr_number)
+  end
+
+  def self.insert_body_into_markdown(lines, body_array, now_string)
     if i = lines.index(now_string)
       lines[0..i] + body_array + lines[i+1..lines.length]
     else
       [now_string] + body_array + lines
-    end
+    end.join("\n\n")
   end
 
-  def self.error_response
-    [500, { 'Content-Type' => 'text/html' }, StringIO.new('Error')]
+  def self.update_html(index_markdown)
+    html_sha = contents_hash(PAGES_REPO_NAME, HTML_FILENAME)[:sha]
+    index_html = RDiscount.new(index_markdown).to_html
+    update(PAGES_REPO_NAME, HTML_FILENAME, COMMIT_MESSAGE, html_sha, index_html, PAGES_BRANCH)
   end
 
-  def self.success_response
-    [200, { 'Content-Type' => 'text/html' }, StringIO.new('')]
+  def self.update_markdown(markdown_sha, index_markdown)
+    update(PAGES_REPO_NAME, MARKDOWN_FILENAME, COMMIT_MESSAGE, markdown_sha, index_markdown, PAGES_BRANCH)
   end
 end
